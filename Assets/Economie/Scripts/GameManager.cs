78db346f416936820601cc61d9ee2198128680b6
ludovicmantovani <ludovic.mantov@gmail.com>
2022-06-23 10:30:26 +0200

    [CHANGE]: sequencing player beaviour in game manager

--- Assets/Economie/Scripts/GameManager.cs
+++ Assets/Economie/Scripts/GameManager.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PlayerInventory _playerInventory;
     [SerializeField] private GameObject _enclosureGO;
+    [SerializeField] private List<LevelScriptableObject> _levels;
+    [SerializeField] private TMP_Text _levelInfo;
+
 
     private Zone _lastTypeZone = Zone.NONE;
 
-    private bool _haveEnclosure = false;
+    private bool _doTransaction = false;
+
+    private int _levelIndex = -1;
+    private int _sequenceIndex = -1;
+    private TransactionScriptableObject _currentTransaction;
 
     void Start()
     {
         if (_enclosureGO)
         {
-            _enclosureGO.SetActive(_haveEnclosure);
+            _enclosureGO.SetActive(false);
         }
         if (_playerInventory)
         {
@@ -26,20 +35,102 @@ public class GameManager : MonoBehaviour
             _playerInventory.Tissu = 0;
             _playerInventory.Argent = 0;
         }
+
+        if (_levels != null && _levels.Count > 0)
+        {
+            DisplayLevelInfo(_levels[0]);
+            if (_levels[0].sequence.Count > 0)
+            {
+                _currentTransaction = _levels[0].sequence[0];
+                _doTransaction = true;
+            }
+        }
+
+
+
+        /*for (int i = 0; i < _levels.Count; i++)
+        {
+            LevelScriptableObject _level = _levels[i];
+
+            
+            for (int x = 0; x < _level.sequence.Count; x++)
+            {
+                TransactionScriptableObject transaction = _level.sequence[x];
+                // Si la destination du joueur 
+            }
+        }*/
     }
+
+
 
     void Update()
     {
-        
+        if (_doTransaction && _currentTransaction)
+        {
+
+            //set player destination to _currentTransaction.zone;
+        }
     }
 
+    private void DisplayLevelInfo(LevelScriptableObject level)
+    {
+        if (_levelInfo)
+        {
+            string levelText = "Niveau " + level.nbr.ToString();
+            _levelInfo.text = levelText;
+            Debug.Log(levelText);
+        }
+    }
     public void OnZoneEnter(Zone typeZone)
     {
-        if (_lastTypeZone == Zone.NONE || _lastTypeZone != typeZone)
+        /**if (_lastTypeZone == Zone.NONE || _lastTypeZone != typeZone)
         {
             _lastTypeZone = typeZone;
             Debug.Log("Entree en zone " + Enum.GetName(typeof(Zone), typeZone));
             MakeTransaction(typeZone);
+        }**/
+
+        if (typeZone == _currentTransaction.zone)
+        {
+            MakeTransaction(_currentTransaction);
+            _doTransaction = NextTransaction();
+            if (_doTransaction && typeZone == _currentTransaction.zone)
+            {
+                OnZoneEnter(typeZone);
+            }
+        }
+    }
+
+    private bool NextTransaction()
+    {
+        if (_levels[_levelIndex].sequence.Count > _sequenceIndex + 1)
+        {
+            _sequenceIndex++;
+        }
+        else if (_levels.Count > _levelIndex + 1 &&
+            _levels[_levelIndex + 1].sequence.Count > 0)
+        {
+            _sequenceIndex = 0;
+            _levelIndex++;
+            DisplayLevelInfo(_levels[_levelIndex]);
+        }
+        else
+        {
+            return false;
+        }
+        _currentTransaction = _levels[_levelIndex].sequence[_sequenceIndex];
+        return true;
+    }
+
+    private void MakeTransaction(TransactionScriptableObject transaction)
+    {
+        MakeTransaction(transaction.zone);
+        if (transaction.outputItem == Item.enclos)
+        {
+            if (_enclosureGO)
+            {
+                _enclosureGO.SetActive(true);
+            }
         }
     }
 
--- Assets/Economie/Scripts/ScriptableObject/LevelScriptableObject.cs
+++ Assets/Economie/Scripts/ScriptableObject/LevelScriptableObject.cs
@@ -7,6 +7,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Level", menuName = "Economy/Level", order = 2)]
 public class LevelScriptableObject : ScriptableObject
 {
-    public int level;
-    public List<TransactionScriptableObject> _sequence;
+    public int nbr;
+    public List<TransactionScriptableObject> sequence;
 }
