4c3a48a1890f374be70bbcdbb1d580b0a4d1f95d
ludovicmantovani <ludovic.mantov@gmail.com>
2022-06-23 22:05:23 +0200

    [CHANGE]: FlyCamera on notification scene

--- Assets/Notification/Scripts/FlyCamera.cs
+++ Assets/Notification/Scripts/FlyCamera.cs
@@ -1,69 +1,83 @@
-using System;
 using UnityEngine;
-using System.Collections;
 
+[RequireComponent(typeof(Camera))]
 public class FlyCamera : MonoBehaviour
 {
+	public float acceleration = 50; // how fast you accelerate
+	public float accSprintMultiplier = 4; // how much faster you go when "sprinting"
+	public float lookSensitivity = 1; // mouse look sensitivity
+	public float dampingCoefficient = 5; // how quickly you break to a halt after you stop your input
+	public bool focusOnEnable = true; // whether or not to focus and lock cursor immediately on enable
 
+	Vector3 velocity; // current velocity
 
+	static bool Focused
+	{
+		get => Cursor.lockState == CursorLockMode.Locked;
+		set
+		{
+			Cursor.lockState = value ? CursorLockMode.Locked : CursorLockMode.None;
+			Cursor.visible = value == false;
+		}
+	}
 
-    public float cameraSensitivity = 90;
-    public float climbSpeed = 4;
-    public float normalMoveSpeed = 10;
-    public float slowMoveFactor = 0.25f;
-    public float fastMoveFactor = 3;
+	void OnEnable()
+	{
+		if (focusOnEnable) Focused = true;
+	}
 
-    private float rotationX = 0.0f;
-    private float rotationY = 0.0f;
+	void OnDisable() => Focused = false;
 
-    void Start()
-    {
-        Screen.lockCursor = true;
-        //Cursor.lockState= CursorLockMode.Locked;
-    }
+	void Update()
+	{
+		// Input
+		if (Focused)
+			UpdateInput();
+		else if (Input.GetMouseButtonDown(0))
+			Focused = true;
 
-    void Update()
-    {
+		// Physics
+		velocity = Vector3.Lerp(velocity, Vector3.zero, dampingCoefficient * Time.deltaTime);
+		transform.position += velocity * Time.deltaTime;
+	}
 
-        //Duplicate 'Mouse X', name it 'Joy X' from InputManager and change Type -> Joystick Axis
-        //Axis -> 4th axis (joystick)
-        //Sensitivity = 1
-        rotationX += Input.GetAxis("Joy X") * cameraSensitivity * Time.deltaTime;
+	void UpdateInput()
+	{
+		// Position
+		velocity += GetAccelerationVector() * Time.deltaTime;
 
-        //Duplicate 'Mouse Y', name it 'Joy Y' from InputManager and change Type -> Joystick Axis
-        //Axis -> 5th axis (joystick)
-        //Sensitivity = 1
-        //Invert = true (check)
-        rotationY += Input.GetAxis("Joy Y") * cameraSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90, 90);
+		// Rotation
+		Vector2 mouseDelta = lookSensitivity * new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+		Quaternion rotation = transform.rotation;
+		Quaternion horiz = Quaternion.AngleAxis(mouseDelta.x, Vector3.up);
+		Quaternion vert = Quaternion.AngleAxis(mouseDelta.y, Vector3.right);
+		transform.rotation = horiz * rotation * vert;
 
-        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+		// Leave cursor lock
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Focused = false;
+	}
 
-        //if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        if (Input.GetKey(KeyCode.Joystick1Button0))
-        {
-            transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
-        {
-            transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
-        }
-        else
-        {
-            transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-            transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        }
+	Vector3 GetAccelerationVector()
+	{
+		Vector3 moveInput = default;
 
+		void AddMovement(KeyCode key, Vector3 dir)
+		{
+			if (Input.GetKey(key))
+				moveInput += dir;
+		}
 
-        if (Input.GetKey(KeyCode.Joystick1Button5)) { transform.position += transform.up * climbSpeed * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.Joystick1Button4)) { transform.position -= transform.up * climbSpeed * Time.deltaTime; }
+		AddMovement(KeyCode.Z, Vector3.forward);
+		AddMovement(KeyCode.S, Vector3.back);
+		AddMovement(KeyCode.D, Vector3.right);
+		AddMovement(KeyCode.Q, Vector3.left);
+		AddMovement(KeyCode.Space, Vector3.up);
+		AddMovement(KeyCode.LeftControl, Vector3.down);
+		Vector3 direction = transform.TransformVector(moveInput.normalized);
 
-        if (Input.GetKeyDown(KeyCode.End))
-        {
-            Screen.lockCursor = (Screen.lockCursor == false) ? true : false;
-        }
-    }
+		if (Input.GetKey(KeyCode.LeftShift))
+			return direction * (acceleration * accSprintMultiplier); // "sprinting"
+		return direction * acceleration; // "walking"
+	}
 }
